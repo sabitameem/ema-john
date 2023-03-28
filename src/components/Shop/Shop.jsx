f7dec93b0b3aaf6f8832c7af9c6e9a706a937250
sabitameem <sabitameem578@gmail.com>
2023-03-28 18:20:17 +0530

    error fixed

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
@@ -6,15 +7,38 @@ import './Shop.css'
 const Shop = () => {
     const [products,setProducts]= useState([])
     const[cart,setCart]=useState([])
+    
     useEffect(()=>{
        fetch('products.json')
        .then(res =>res.json())
        .then(data=> setProducts(data))
     },[])
 
+    
+    useEffect( () =>{
+      const storedCart = getShoppingCart();
+      const savedCart = [];
+      // step 1: get id of the addedProduct
+      for(const id in storedCart){
+          // step 2: get product from products state by using id
+          const addedProduct = products.find(product => product.id === id)
+          if(addedProduct){
+              // step 3: add quantity
+              const quantity = storedCart[id];
+              addedProduct.quantity = quantity;
+              // step 4: add the added product to the saved cart
+              savedCart.push(addedProduct);
+          }
+          // console.log('added Product', addedProduct)
+      }
+      // step 5: set the cart
+      setCart(savedCart);
+  }, [products])
+
     const handleToCart =(product)=>{
         const newCart =[...cart,product]
         setCart(newCart)
+        addToDb(product.id)
     }
 // console.log(cart)
     return (
