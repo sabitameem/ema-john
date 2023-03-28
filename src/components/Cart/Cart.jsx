85d24564b375fecee6ca253794e2f382b22935c5
sabitameem <sabitameem578@gmail.com>
2023-03-28 18:35:14 +0530

    cart fully addedin local-storage

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,25 +1,38 @@
 import React from 'react';
-import './Cart.css'
+import './Cart.css';
 
-const Cart = (props) => {
-   const cart=props.cart;
-   let total = 0;
-   let totalShipping =0;
-   for(const product of cart){
-    total = total + product.price;
-    totalShipping = totalShipping + product.shipping;
-   }
-   const tax =total*7/100;
-   const grandTotal = total + totalShipping + tax;
+const Cart = ({ cart }) => {
+    // const cart = props.cart; // option 1
+    // const {cart} = props; // option 2
+
+    // console.log(cart);
+
+    let totalPrice = 0;
+    let totalShipping = 0;
+    let quantity = 0;
+    for (const product of cart) {
+        // if(product.quantity === 0){
+        //     product.quantity = 1;
+        // }
+        // product.quantity = product.quantity || 1;
+
+        totalPrice = totalPrice + product.price * product.quantity;
+        totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
+    }
+    const tax = totalPrice * 7 / 100;
+
+    const grandTotal = totalPrice + totalShipping + tax;
+
     return (
         <div className='cart'>
-        <h2>Order Summary</h2>
-        <p>Selected Items:{cart.length} </p>
-        <p>Total Price: ${total}</p>
-        <p>Shipping: ${totalShipping}</p>
-        <p>Tax: ${tax.toFixed(2)}</p>
-        <h3>Grand Total: ${grandTotal.toFixed(2)}</h3>
-    </div>
+            <h2>Order Summary</h2>
+            <p>Selected Items: {quantity}</p>
+            <p>Total Price: ${totalPrice}</p>
+            <p>Shipping: ${totalShipping}</p>
+            <p>Tax: ${tax.toFixed(2)}</p>
+            <h3>Grand Total: ${grandTotal.toFixed(2)} </h3>
+        </div>
     );
 };
 
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -35,11 +35,26 @@ const Shop = () => {
       setCart(savedCart);
   }, [products])
 
-    const handleToCart =(product)=>{
-        const newCart =[...cart,product]
-        setCart(newCart)
-        addToDb(product.id)
+  const handleToCart = (product) => {
+    // cart.push(product); '
+    let newCart = [];
+    // const newCart = [...cart, product];
+    // if product doesn't exist in the cart, then set quantity = 1
+    // if exist update quantity by 1
+    const exists = cart.find(pd => pd.id === product.id);
+    if(!exists){
+        product.quantity = 1;
+        newCart= [...cart, product]
     }
+    else{
+        exists.quantity = exists.quantity + 1;
+        const remaining = cart.filter(pd => pd.id !== product.id);
+        newCart = [...remaining, exists];
+    }
+
+    setCart(newCart);
+    addToDb(product.id)
+}
 // console.log(cart)
     return (
         <div className='shop-container'>
